ca765f1f66148359c65fcf4acc119685db81bcf8
Benjamin Gelb <beg5670@gmail.com>
2016-04-30 06:38:27 -0400

    core files

---
+++ env/install.sh
@@ -0,0 +1,9 @@
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# tmux plugin manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# vim plugin manager
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
