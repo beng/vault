b6325b4cd236dd132f599c4144eba60780cc8b37
Benjamin Gelb <beg5670@gmail.com>
2016-08-26 21:02:04 -0400

    download and setup neovim

--- env/install.sh
+++ env/install.sh
@@ -7,3 +7,7 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 # vim plugin manager
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# neovim plugin manager
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
